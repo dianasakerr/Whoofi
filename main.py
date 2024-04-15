61fda50b11ad203ecff2b7c7453c5033a80663ba
dianasakerr <diansaker>
2024-04-15 11:44:39 +0300

    added mongo db

--- main.py
+++ main.py
@@ -4,9 +4,22 @@ import uvicorn
 from fastapi import FastAPI, HTTPException, Form
 from pydantic import BaseModel, Field, field_validator
 from typing import List
+from pymongo import MongoClient
+from abc import abstractmethod
 
 app = FastAPI()
 users = []
+CONN_STR = "mongodb+srv://dianasakeer:1234@cluster-1.xgxy3zr.mongodb.net/"
+DB_NAME = 'Whoofi'
+DOG_OWNER = 'dog_owner'
+DOG_WALKER = 'dog_walker'
+
+
+def get_collction_from_db(collection_name):
+    # Create a new client and connect to the server
+    cluster = MongoClient(CONN_STR)
+    db = cluster[DB_NAME]
+    return db[collection_name], cluster
 
 
 class User(BaseModel):
@@ -29,6 +42,11 @@ class User(BaseModel):
             raise ValueError("must contain a valid email address")
         return v
 
+    @abstractmethod
+    def save_user(self):
+        return {'_id': self.id, 'username': self.username, 'email': self.email,
+                'city': self.city, 'region': self.region}
+
 
 class DogOwner(User):
     dogs: List[str]
@@ -36,11 +54,23 @@ class DogOwner(User):
     dog_birth_date: str
     dog_type: str
 
+    def __init__(self, **values):
+        super().__init__(**values)
+        self.save_user()
+
     @field_validator("dogs")
     def dog_name_must_not_be_empty(cls, v):
         if not v.strip():
             raise ValueError("dog name cannot be empty")
         return v
+
+    def save_user(self):
+        data = super(DogOwner, self).save_user()
+        data.update({'dogs': self.dogs, 'dog_name': self.dog_name, 'dog_birth_date': self.dog_birth_date,
+                     'dog_type': self.dog_type})
+        collection, cluster = get_collction_from_db(DOG_OWNER)
+        collection.insert_one(data)
+        cluster.close()
 
 
 class DogWalker(User):
@@ -50,11 +80,23 @@ class DogWalker(User):
     age: int
     years_of_experience: float
 
+    def __init__(self, **values):
+        super().__init__(**values)
+        self.save_user()
+
     @field_validator("hourly_rate")
     def rate_must_be_positive(cls, v):
         if v <= 0:
             raise ValueError("hourly rate must be positive")
         return v
+
+    def save_user(self):
+        data = super(DogWalker, self).save_user()
+        data.update({'services': self.services, 'hourly_rate': self.hourly_rate, 'name': self.name,
+                     'age': self.age, 'years_of_experience': self.years_of_experience})
+        collection, cluster = get_collction_from_db(DOG_WALKER)
+        collection.insert_one(data)
+        cluster.close()
 
 
 @app.get("/")
