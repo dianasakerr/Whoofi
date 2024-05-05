99909e135040ca317ff0a68433019039901309e3
dianasakerr <dianasakeer@gmail.com>
2024-05-05 11:39:35 +0300

    changed classes attributed and added Dog class and create_dog methond

--- back/main.py
+++ back/main.py
@@ -3,10 +3,9 @@ import webbrowser
 import uvicorn
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, field_validator
-from typing import List, Dict
+from typing import Dict, Optional
 from pymongo import MongoClient
 from abc import abstractmethod
-from fastapi.param_functions import Depends
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -15,11 +14,9 @@ CONN_STR = "mongodb+srv://dianasakeer:1234@cluster-1.xgxy3zr.mongodb.net/"
 DB_NAME = 'Whoofi'
 DOG_OWNER = 'dog_owner'
 DOG_WALKER = 'dog_walker'
+DOG = 'dog'
 
-origins = [
-    "http://localhost",
-    "http://localhost:5173"
-]
+origins = ["http://localhost", "http://localhost:5173"]
 
 # Add CORS middleware to your FastAPI application
 app.add_middleware(
@@ -30,9 +27,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 def get_collction_from_db(collection_name):
     # Create a new client and connect to the server
     cluster = MongoClient(CONN_STR)
+    # Send a ping to confirm a successful connection
+    try:
+        cluster.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+
     db = cluster[DB_NAME]
     return db[collection_name], cluster
 
@@ -41,74 +46,96 @@ class User(BaseModel):
     id: int
     username: str
     email: str
+    address: str
     city: str
     region: str
+    phone_number: int
 
     class Config:
         from_attributes = True
 
-    # @classmethod
-    # def __init__(cls, **values):
-    #     super().__init__(**values)
-
-    @field_validator("email")
-    def email_must_contain_at(cls, v):
-        if "@" not in v:
-            raise ValueError("must contain a valid email address")
-        return v
-
     @abstractmethod
     def save_user(self):
-        return {'_id': self.id, 'username': self.username, 'email': self.email,
-                'city': self.city, 'region': self.region}
+        return {'_id': self.id, 'username': self.username, 'email': self.email, 'address': self.address,
+                'city': self.city, 'region': self.region, 'phone_number': self.phone_number}
+
+
+class Dog(BaseModel):
+    name: str
+    age: float
+    race: str
+    owner_id: int
+    weight: float  # in kg
+    size: Optional[str] = None
+
+    def __init__(self, **values):
+        super(Dog, self).__init__(**values)
+        self.size = self.get_size()
+        self.save_user()
+
+    def get_size(self):
+        if self.weight <= 10:
+            return 'small'
+        if self.weight <= 25:
+            return 'medium'
+        return 'big'
+
+    # @field_validator("name")
+    # def dog_name_must_not_be_empty(cls, v):
+    #     if not v.strip():
+    #         raise ValueError("dog name cannot be empty")
+    #     return v
+
+    def save_user(self):
+        # check if owner_id exists in dog_owner collection
+        dog_owner_collection, cluster = get_collction_from_db(DOG_OWNER)
+        owner_exists = dog_owner_collection.find_one({"_id": self.owner_id}) is not None
+        cluster.close()
+
+        if not owner_exists:
+            raise ValueError(f"Owner with ID {self.owner_id} does not exist.")
+
+        data = {'name': self.name, 'age': self.age, 'race': self.race, 'owner_id': self.owner_id, 'weight': self.weight,
+                'size': self.size}
+        collection, cluster = get_collction_from_db(DOG)
+        collection.insert_one(data)
+        cluster.close()
+
+        # update dog owner dogs list
+        collection, cluster = get_collction_from_db(DOG_OWNER)
+        # TODO: check if to save unique names of the dogs owner or to save dog in dogs list by id
+        collection.update_one(filter={'_id': self.owner_id}, update={'$push': {'dogs': [self.name]}})
+        cluster.close()
 
 
 class DogOwner(User):
-    dogs: List[str]
-    dog_name: str
-    dog_birth_date: str
-    dog_type: str
+    dogs:  list  # updated automatically when creating dog
 
     def __init__(self, **values):
         super().__init__(**values)
         self.save_user()
 
-    @field_validator("dog_name")
-    def dog_name_must_not_be_empty(cls, v):
-        if not v.strip():
-            raise ValueError("dog name cannot be empty")
-        return v
-
     def save_user(self):
         data = super(DogOwner, self).save_user()
-        data.update({'dogs': self.dogs, 'dog_name': self.dog_name, 'dog_birth_date': self.dog_birth_date,
-                     'dog_type': self.dog_type})
+        data['dogs'] = self.dogs
         collection, cluster = get_collction_from_db(DOG_OWNER)
         collection.insert_one(data)
         cluster.close()
 
 
 class DogWalker(User):
-    services: List[str]
-    hourly_rate: float
-    name: str
-    age: int
+    age: float
     years_of_experience: float
+    hourly_rate: float
 
     def __init__(self, **values):
         super().__init__(**values)
         self.save_user()
 
-    @field_validator("hourly_rate")
-    def rate_must_be_positive(cls, v):
-        if v <= 0:
-            raise ValueError("hourly rate must be positive")
-        return v
-
     def save_user(self):
         data = super(DogWalker, self).save_user()
-        data.update({'services': self.services, 'hourly_rate': self.hourly_rate, 'name': self.name,
-                     'age': self.age, 'years_of_experience': self.years_of_experience})
+        data.update({'hourly_rate': self.hourly_rate, 'age': str(self.age),
+                     'years_of_experience': self.years_of_experience})
         collection, cluster = get_collction_from_db(DOG_WALKER)
         collection.insert_one(data)
         cluster.close()
@@ -119,25 +146,36 @@ async def read_root():
     return {"message": "Welcome to the Whoofi API"}
 
 
-class CustomForm(BaseModel):
+class UserCustomForm(BaseModel):
     user_type: str
     user_data: Dict
 
 
 @app.post("/create_user/")
-async def create_user(form_data: DogOwner):
-    user_type = form_data.user_type
+async def create_user(form_data: UserCustomForm):
+    user_type = form_data.user_type.lower()
     user_data = form_data.user_data
-    if user_type.lower() == 'dogowner':
+    if user_type.lower() == 'owner':
         user = DogOwner(**user_data)
-    elif user_type.lower() == 'dogwalker':
+    elif user_type.lower() == 'walker':
         user = DogWalker(**user_data)
     else:
-        raise HTTPException(status_code=400, detail="Invalid user type. Please specify 'dogowner' or 'dogwalker'.")
+        raise HTTPException(status_code=400, detail="Invalid user type. Please specify 'owner' or 'walker'.")
 
     users.append(user)
     return {"message": f"Hello {user.username}, your data is: {user.dict()}"}
 
+
+class DogCustomForm(BaseModel):
+    user_data: Dict
+
+
+@app.post("/create_dog/")
+async def create_dog(form_data: DogCustomForm):
+    dog_data = form_data.user_data
+    dog = Dog(**dog_data)
+    # TODO:  add the dog to user dogs list
+    return {"message": f"Hello {dog.name}, your data is: {dog_data}"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="localhost", port=8000, reload=True, log_level="info")
