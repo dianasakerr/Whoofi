007e8e1629b0d950d41f7dd001fde9e6f53eef31
guy pridan <guy.pridan@gmail.com>
2024-05-15 21:21:07 +0300

    merging frontend work to main branch

---
+++ frontend/src/SignupProcess.tsx
@@ -0,0 +1,121 @@
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import ChooseAccountType from './SignupProcessComponents/ChooseAccountType';
+import EnterEmail from './SignupProcessComponents/EnterEmail';
+import SetPassword from './SignupProcessComponents/SetPassword';
+import SetName from './SignupProcessComponents/SetName';
+import SubmitPage from './SignupProcessComponents/SubmitPage';
+import SetLocation from './SignupProcessComponents/SetLocation';
+import './styles/SignupProcess.css'
+
+interface Location {
+  lat: number;
+  lng: number;
+}
+
+function SignupProccess() {
+  const [name,setName] = useState<string>("");
+  const [accountType, setAcountType] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [address, setAddress] = useState<string>("");
+  const [location, setLocation] = useState<Location | null>(null);
+
+  
+
+  const onSubmit = async () => {
+    fetch('http://localhost:8000/create_user/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            user_type: accountType, // Make sure this is directly under the root of the JSON body
+            user_data: accountType === 'owner' ? {
+              id: uuidv4(),
+              username: name,
+              email: email,
+              password: password,
+              location: JSON.stringify(location),
+              address: address,
+              city: "undefined",
+              region: "undefined",
+              phone_number: -1,
+              dogs: ['dog1','dog2']
+            } : {
+              id: uuidv4(),
+              username: name,
+              email: email,
+              password: password,
+              address: address,
+              location: JSON.stringify(location),
+              city: "undefined",
+              region: "undefined",
+              phone_number: -1,
+              hourly_rate: 35.2,
+              years_of_experience: 3,
+              age: "17"
+            }
+
+        })
+    }).then(res => {
+        if (res.ok) {
+            console.log("User created successfully");
+            return res.json();
+        } else {
+            console.log("Error creating user");
+            return res.text().then(text => { throw new Error(text) });
+        }
+    }).then(data => {
+        console.log(data);
+    }).catch(err => {
+        console.error("my error log:", err);
+    });
+}
+
+  return (
+    <div className='signup-container'>
+    <h1 className="title">Woofi signup process</h1>
+    
+    {
+      (accountType === "") &&
+      <ChooseAccountType setAccountType={setAcountType}/>
+    }
+    
+
+    { 
+      accountType !== "" &&
+      email === "" &&
+     <EnterEmail setEmail={setEmail}/>
+    }
+
+    { email !== "" &&
+      password === "" &&
+      <SetPassword setPassword={setPassword}/>
+    }
+
+    {
+      email !== "" &&
+      password !== "" &&
+      name === "" &&
+      <SetName setName={setName}/>
+    }
+
+    {
+      email !== "" &&
+      password !== "" &&
+      name !== "" &&
+      !location &&
+      <SetLocation setFinalLocation={setLocation} setFinalAddress={setAddress}/>
+    }
+
+    { email !== "" &&
+    password !== "" &&
+    name !== "" && 
+    location && 
+    <SubmitPage name={name} email={email} onSubmit={onSubmit} setEmail={setEmail} setName={setName}/>}
+    </div>
+  )
+}
+
+export default SignupProccess;
---
+++ frontend/src/SignupProcessComponents/LocationInputMap.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
+import AddressSearchBar from './AddressSearchBar';
+import 'leaflet/dist/leaflet.css';
+
+function LocationMarker({ setLocation }) {
+  const map = useMapEvents({
+    click(e) {
+      setLocation(e.latlng);
+    },
+  });
+  
+  return null;
+}
+
+function LocationInputMap({ setFinalLocation }) {
+  const [map, setMap] = useState(null);
+  const [location, setLocation] = useState(null);
+  const defaultLocation = { lat: 32.077976, lng: 34.774220} // set to TLV's dizingof squere
+
+  
+
+  useEffect(() => {
+    if (map && location) {
+        map.setView([location.lat,location.lng]);
+    }
+    else {
+      console.log(map,location);
+    }
+}, [map, location]);
+
+  return (
+    <>
+    <AddressSearchBar setLocation={setLocation}/>
+    <MapContainer center={location ? [location.lat, location.lng] : [defaultLocation.lat,defaultLocation.lng]} zoom={13} style={{ height: '400px', width: '100%' }} ref={setMap}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {location && <Marker position={location} />}
+      <LocationMarker setLocation={setLocation} />
+    </MapContainer>
+    <button onClick={() => setFinalLocation(location)}>confirm manual choice</button>
+    </>
+
+  );
+}
+
+export default LocationInputMap;
