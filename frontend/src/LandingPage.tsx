4a3bb1613f0caf3babe8473be6104dbb835a60cd
guypridan <guy.pridan@gmail.com>
2024-05-16 19:17:12 +0300

    added frontend request loaction and size filter

--- frontend/src/LandingPage.tsx
+++ frontend/src/LandingPage.tsx
@@ -1,4 +1,5 @@
 import Header from "./Header"
+import Login from "./Login"
 
 const LandingPage = () => {
   return (
@@ -8,6 +9,7 @@ const LandingPage = () => {
     <p>
         Welcome to Woofi, where wagging tails and happy walks meet! this platform is designed to connect passionate dog walkers with loving dog owners. Whether you're a dedicated pet parent seeking reliable care for your furry friend or an enthusiastic dog lover eager to embark on joyful adventures with canine companions, you've come to the right place. We will help you to find perfect match, ensuring that every walk is not just a stroll but an experience filled with care, trust, and tail-wagging delight. Join us today and let's make tails wag together!
     </p>
+    <Login/>
     </>
   )
 }
