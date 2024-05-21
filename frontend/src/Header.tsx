4b0c28033b766e7af345403eccc30f04240c1ddf
guy pridan <guy.pridan@gmail.com>
2024-05-21 14:57:50 +0300

    header is updated based on if a user is logged in

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -1,18 +1,44 @@
 import { Link } from 'react-router-dom';
 import "./styles/Header.css";
 import Logo from './Logo';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
+  const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
+
+  const checkLocalStorage = () => {
+    const item = localStorage.getItem('token');
+    setIsSignedIn(!!item);
+  };
+
+  useEffect(() => {
+    checkLocalStorage();
+
+    const handleStorageChange = () => {
+      console.log('storage change heard from header');
+      checkLocalStorage();
+    };
+
+    window.addEventListener('storage', handleStorageChange);
+
+    return () => {
+      window.removeEventListener('storage', handleStorageChange);
+    };
+  }, []);
+
   return (
     <header className="header">
       <Logo />
       <nav className="navbar">
         <ul className="nav-list">
-          <li><Link to="/profile" className="nav-link">My Profile</Link></li>
+          {isSignedIn ? 
+            <li><Link to="/profile" className="nav-link">My Profile</Link></li> :
+            <li><Link to="/login" className="nav-link">Sign in</Link></li>
+          }
           <li><Link to="/signup" className="nav-link">SignUp</Link></li>
           <li><Link to="/" className="nav-link">Home Page</Link></li>
           <li><Link to="/search" className="nav-link">Look for a Dog Walker</Link></li>
-          <li><a href="#" className="nav-link" onClick={() => { /* Handle Exit */ }}>Exit</a></li>
+          <li><a className="nav-link" onClick={() => {localStorage.removeItem('token'); window.dispatchEvent(new Event('storage'));}}>Exit</a></li>
         </ul>
       </nav>
     </header>
