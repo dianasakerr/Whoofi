95f965fc56581733443aa154570103105936da65
guy pridan <guy.pridan@gmail.com>
2024-05-23 14:42:24 +0300

    add redirection to whatsapp

--- frontend/src/DogWalkerProfile.jsx
+++ frontend/src/DogWalkerProfile.jsx
@@ -2,12 +2,37 @@ import React, { useState, useEffect } from 'react';
 import './styles/styles.css'; // Import your CSS styles
 import woofiLogo from './logosAndIcons/woofiLogo.jpeg'
 import backArrow from './logosAndIcons/back-arrow.svg'
+import whatsappLogo from './logosAndIcons/whatsapp logo.png'
+import axios from 'axios';
 
 
 
 const DogWalkerProfile = ({dogWalker, setCurrentDogWalker}) => {
+  const [address, setAddress] = useState("");
 
   const resetDogWalker = () => setCurrentDogWalker(null)
+
+
+  const reverseGeocode = async (lat, lng) => {
+    try {
+      const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}`);
+      response.json().then(data => {
+        setAddress(data.address);
+      })
+    } catch (error) {
+      setAddress('Address not found');
+    }
+  };
+
+  useEffect(() => {
+    reverseGeocode(...dogWalker.coordinates);
+    console.log(dogWalker);
+  },[]);
+
+  const gotoWhatsap = () => {
+    const internetional = "+972"+ dogWalker.phone_number.replace(/^0/, '');
+    window.open("https://wa.me/" + internetional);
+  }
 
   return (
     <div className="container">
@@ -18,16 +43,15 @@ const DogWalkerProfile = ({dogWalker, setCurrentDogWalker}) => {
         <img src={woofiLogo} alt={dogWalker.name} className='profile-image' height="300" width="300"/>
         <div className="details">
           <h2>{dogWalker.username}</h2>
-          <p>Location: {}</p>
-          <p>Experience: {dogWalker.experience} years</p>
-          <p>Price: ${dogWalker.price} per hour</p>
-          <p>Phone: {dogWalker.phoneNumber}</p>
+          <p>Location: {address.city || address.town}, {address.road} {address.house_number}</p>
+          <p>Experience: {dogWalker.years_of_experience} years</p>
+          <p>Price: ${dogWalker.hourly_rate} per hour</p>
+          <p>Phone: {dogWalker.phone_number} <img src={whatsappLogo} alt="" onClick={gotoWhatsap} height="30" width="30"/></p>
           <p>Rating: {dogWalker.rating}</p>
         </div>
       </div>}
       <img src={backArrow} alt="" className="src" onClick={resetDogWalker} height="30" width="30"/>
     </div>
-    
   );
 };
 
