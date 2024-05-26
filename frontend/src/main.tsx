fce05dbe233097483a92733330a551fe3d6b2834
Guy pridan <guy.pridan@gmail.com>
2024-05-26 22:38:57 +0300

    fixed returned status code from sign in request, transfered elements to mui components for better design

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import WindowSetter from './WindowSetter.tsx';
+import { ThemeProvider } from '@emotion/react';
+import theme from './styles/theme.js'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
+        <ThemeProvider theme={theme}>
         <Router>
             <WindowSetter />
         </Router>
+        </ThemeProvider>
     </React.StrictMode>
 );
