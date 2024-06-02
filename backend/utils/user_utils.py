357bb43d7c0ff6fabde6509438d0c97b0881c487
dianasakerr <dianasakeer@gmail.com>
2024-06-02 21:58:54 +0300

    added whatsp link

--- backend/utils/user_utils.py
+++ backend/utils/user_utils.py
@@ -6,3 +6,9 @@ def calculate_age(date_of_birth: str):
     birth_date = datetime.strptime(date_of_birth, "%Y-%m-%d")
     age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
     return age
+
+
+def generate_whatsapp_link(phone_number: str) -> str:
+    if phone_number.startswith('0'):
+        phone_number = phone_number[1:]
+    return f"https://api.whatsapp.com/send/?phone={phone_number}&text&type=phone_number&app_absent=0"
