1516dcc2a98cc4dbb1ac9d3ed1182f3fb49cb565
‪Batya ohayon Gabay‬‏ <batyaoh@gmail.com>
2024-06-14 13:30:20 +0300

    Added get_dogs_by_user function to dog_routes

--- backend/routes/dog_routes.py
+++ backend/routes/dog_routes.py
@@ -17,3 +17,24 @@ async def create_dog(form_data: DogCustomForm):
     dog = Dog(**dog_data)
     # TODO: add the dog to user dogs list
     return {"message": f"Hello {dog.name}, your data is: {dog_data}"}
+
+
+@dog_router.get("/get_dogs_by_user/")
+async def get_dogs_by_user(user_email: str, token: str):
+    # Verify token
+    data = verify_token(token)
+    if data is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token",
+                            headers={"WWW-Authenticate": "Bearer"})
+
+    # Get the dogs associated with the owner email
+    dog_collection, dog_cluster = get_collection(DOG)
+
+    try:
+        dogs = list(dog_collection.find({OWNER_EMAIL: user_email}, {ID: 0}))
+        calc_data_to_users(dogs)
+        if not dogs:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No dogs found for this owner")
+        return dogs
+    finally:
+        dog_cluster.close()
